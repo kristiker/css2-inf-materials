2b592259d9d7e436c2f868b5fa564089cf5da466
kristiker <kristik06@outlook.com>
2022-06-10 15:15:04 +0200

    track custom resources

--- gitinclude-changes.py
+++ gitinclude-changes.py
@@ -3,6 +3,7 @@ from os import stat
 from zlib import crc32
 
 includeFiles = []
+included_but_missing = []
 here = Path('.')
 
 def txt_default_normal_flip(fpath: Path):
@@ -13,12 +14,24 @@ def txt_default_normal_flip(fpath: Path):
     return ('%08X' % (crc & 0xFFFFFFFF)) in ('69D57F2B', '42F60AFF', '005B1D71')
 
 if Path(".gitignore").is_file():
-    with open(".gitignore") as fr:
-        for line in fr.readlines():
+    with open(".gitignore") as fp:
+        for line in fp.readlines():
             line = line.strip()
             if not any(line.endswith(ext) for ext in (".vmat", ".txt")) or "*" in line:
                 continue
-            includeFiles.append(Path(line.strip("!")))
+            file = Path(line.strip("!"))
+            includeFiles.append(file)
+            if not file.exists():
+                included_but_missing.append(file)
+
+if Path("custom_resources.txt").is_file():
+    with Path("custom_resources.txt").open() as fp:
+        for line in fp.readlines():
+            line = line.strip()
+            custom_resource = Path(line)
+            if custom_resource not in includeFiles:
+                includeFiles.append(custom_resource)
+        
     
 for vmat_file in here.glob("**/*.vmat"):
     print("Checking", vmat_file)
@@ -35,8 +48,9 @@ for txt_file in here.glob("**/*.txt"):
     if txt_file not in includeFiles:
         includeFiles.append(txt_file)
 
-with open(".gitignore", "w") as fw:
-    fw.writelines(
+
+with open(".gitignore", "w") as fp:
+    fp.writelines(
         ext + "\n" for ext in [
             "*.vmat",
             "*.tga",
@@ -49,4 +63,9 @@ with open(".gitignore", "w") as fw:
             "*.7z",
         ]
     )
-    fw.writelines(f"!{file.as_posix()}\n" for file in includeFiles)
+    fp.writelines(f"!{file.as_posix()}\n" for file in includeFiles)
+
+if included_but_missing:
+    print("Files missing from disk but included in git:")
+    for file in included_but_missing:
+        print(file.as_posix())
